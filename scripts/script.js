a001093fb9fdd4e933c7f295a17eb93453ae1e48
Fatuh Sani Ansori <162855266+fatuhsa@users.noreply.github.com>
2024-03-11 21:09:00 +0700

    update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -49,7 +49,7 @@ var content2 = document.getElementById('content2');
 if (content2) {
   // Array untuk ikon-ikon baru
   var icons2 = [
-    { name: 'Nama Ikon 1', path: '../icon/8bitemblem.webp' },
+    { name: '8-Bit Emblem', path: '../icon/8bitemblem.webp' },
     { name: 'Nama Ikon 2', path: 'path/ikon2.webp' },
     // Tambahkan ikon-ikon baru di sini
   ];
