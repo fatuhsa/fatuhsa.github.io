3d2deae2dc970011bfd7584a59b43d76ec926b96
Fatuh Sani Ansori <162855266+fatuhsa@users.noreply.github.com>
2024-03-11 09:19:27 +0700

    update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,45 @@
-// Fungsi untuk menampilkan atau menyembunyikan konten saat paragraf diklik
 function toggleContent(menuNumber) {
-  // Menentukan id konten yang sesuai dengan nomor menu yang diklik
   var contentId = "content" + menuNumber;
   var content = document.getElementById(contentId);
-
-  // Memeriksa apakah konten sudah terbuka
   var isOpen = content.style.display === "block";
-
-  // Menyembunyikan semua konten terlebih dahulu
   var allContents = document.querySelectorAll(".opsi");
   allContents.forEach(function(content) {
     content.style.display = "none";
   });
-
-  // Jika konten belum terbuka, buka; jika sudah terbuka, tutup kembali
   if (!isOpen) {
+    var menu = document.getElementById("menu");
+    var menuPosition = menu.getBoundingClientRect();
     content.style.display = "block";
+    content.style.position = "absolute";
+    content.style.top = menuPosition.bottom + "px";
+    content.style.left = menuPosition.left + "px";
+    content.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    content.style.padding = "10px";
   }
+}
+var content1 = document.getElementById('content1');
+
+if (content1) {
+  var icons = [
+    { name: 'B.F. Sword',            path: '../icon/bf-sword.webp' },
+    { name: 'Chain Vest',            path: '../icon/chain-vest.webp' },
+    { name: 'Giant\'s Belt',         path: '../icon/giants-belt.webp' },
+    { name: 'Needlessly Large Rod',  path: '../icon/needlessly-large-rod.webp' },
+    { name: 'Negatron Cloak',        path: '../icon/negatron-cloak.webp' },
+    { name: 'Recurve Bow',           path: '../icon/recurve-bow.webp' },
+    { name: 'Sparring Gloves',       path: '../icon/sparring-gloves.webp' },
+    { name: 'Spatula',               path: '../icon/spatula.png' },
+    { name: 'Tear of the Goddess',   path: '../icon/tear-of-the-goddess.png' }
+    // Tambahkan nama dan path ikon lainnya di sini
+  ];
+
+  var paragraphs = content1.querySelectorAll('p');
+  paragraphs.forEach(function(paragraph, index) {
+    var iconSpan = document.createElement('span');
+    iconSpan.classList.add('icon');
+    iconSpan.style.backgroundImage = 'url(' + icons[index].path + ')';
+    paragraph.insertBefore(iconSpan, paragraph.firstChild);
+  });
+} else {
+  console.error("Elemen dengan id 'content1' tidak ditemukan.");
 }
